1ccc65dedc839bd6993f0b3b071e2bd333d21b0f
Kenny <kenny-williamsStockd@live.nmit.ac.nz>
2021-12-22 14:06:25 +1300

    object literal improvements with es5&6 examples

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,34 @@ function multiply(a: number, b = 25) {
   return a * b;
 }
 
-console.log(multiply(5))
-console.log(multiply(5, 35))
+console.log(multiply(5));
+console.log(multiply(5, 35));
 
-//object literal improvements example
+//object literal improvements example testing with es6 example function 
+const pizza1 = {
+  name: "Pepperoni",
+  price: 15,
+  getName() {
+    return this.name
+  }
+};
+
+const toppings1 = ["pepperoni"];
+
+//es5 example
+// const order = {
+//   pizza: pizza,
+//   toppings: toppings,
+// }
+
+// es6 example
+// const order = {pizza1, toppings1}
+
+// console.log(order)
+
+//using function example
+function createOrder(_pizza: { name: string; price: number; }, _toppings: string[]) {
+  return { pizza1, toppings1 };
+}
+
+console.log(createOrder(pizza1, toppings1));
